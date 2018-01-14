5fa123bfedf46a56be1309809201efc27221f10b
fawad1997 <30860709+fawad1997@users.noreply.github.com>
2018-01-14 15:28:06 +0500

    simplified migration

--- FinalWebProject/Data/Migrations/20180114102614_simplified.Designer.cs
+++ FinalWebProject/Data/Migrations/20180114102614_simplified.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FinalWebProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180114102614_simplified")]
+    partial class simplified
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
