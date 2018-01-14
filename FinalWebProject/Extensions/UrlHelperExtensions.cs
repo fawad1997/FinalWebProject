f7c4669de0e8f50807b67d42e055e7c102e9dbc3
fawad1997 <30860709+fawad1997@users.noreply.github.com>
2018-01-14 13:44:35 +0500

    Add project files.

---
+++ FinalWebProject/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalWebProject.Controllers;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public static class UrlHelperExtensions
+    {
+    }
+}
